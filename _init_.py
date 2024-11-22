f6c417bce18fdd140f597a3694df43606b619191
aloysius <aloysiuslow009@gmail.com>
2024-11-22 09:35:23 +0800

    new files added(empty file)

--- _init_.py
+++ _init_.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Lets do this"
+    return render_template("Login/login.html")
 
 if __name__ == "__main__":
     app.run()
