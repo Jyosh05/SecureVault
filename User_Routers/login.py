6bdebd57c8babaf93158609d60ba94d55c5af704
aloysius <aloysiuslow009@gmail.com>
2024-11-22 16:38:13 +0800

    can use CSRF now, small bug but not graded in login page

--- User_Routers/login.py
+++ User_Routers/login.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,session,flash,Blueprint, redirect, url_for
+from flask import render_template,request,session,flash,Blueprint, redirect, url_for, make_response
 import secrets
 
 login_bp = Blueprint('login', __name__, template_folder='templates')
@@ -9,18 +9,17 @@ def login():
         form_token = request.form.get('csrf_token',None)
         username = request.form.get('username')
         password = request.form.get('password')
-        # need to query the sql db for credentials
-        print(token)
+        session['username'] = username
         if not token or token != form_token:
             flash("CSRF token invalid", 'error')
             return redirect(url_for("login.login"))
         return  redirect(url_for("login.home"))
     session['csrf_token'] = secrets.token_hex(16)
-    print(session['csrf_token'])
     return render_template("Login/login.html", csrf_token=session['csrf_token'])
 
 
 @login_bp.route("/home",)
 def home():
-
+    session.pop('csrf_token',None)
+    print(session)
     return render_template("Login/home.html")
