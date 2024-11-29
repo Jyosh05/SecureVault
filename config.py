3b4ff4ea6febdf90c41ea25e266316f31b942e13
aloysius <aloysiuslow009@gmail.com>
2024-11-29 11:35:18 +0800

    DBconfig

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import secrets
 secret_key = secrets.token_hex(32)
 
 DB_Config = {
-    #to be decided
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'securevault1234',
+    'database': 'securevault',
+    'port': 3306
 }
