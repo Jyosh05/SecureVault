8782846e024349c661c89ce443477091800112f7
aloysius <aloysiuslow009@gmail.com>
2024-12-05 16:15:04 +0800

    DB setup

--- _init_.py
+++ _init_.py
@@ -1,16 +1,22 @@
 from flask import Flask
-from config import secret_key
+from config import secret_key, DB_Config
 from User_Routers.login import login_bp
 from User_Routers.register import register_bp
 from User_Routers.User_Functions import User_Features
+import mysql.connector
+from general_utils import mydb, check_table
 
 
 app = Flask(__name__)
+#app secret key
 app.secret_key = secret_key
 
+
+#app routes
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(User_Features)
 
 if __name__ == "__main__":
+    check_table()
     app.run(debug=True)
---
+++ general_utils.py
@@ -0,0 +1,140 @@
+import mysql.connector
+from config import DB_Config
+
+mydb = mysql.connector.connect(
+    host=DB_Config['host'],
+    user=DB_Config['user'],
+    password=DB_Config['password'],
+    port=DB_Config['port'],
+    database=DB_Config['database']
+)
+
+def check_table():
+    mycursor = mydb.cursor(buffered=True)
+
+    try:
+        # Check and create user table
+        mycursor.execute(f"SHOW TABLES LIKE 'user'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS user(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    Username VARCHAR(255) NOT NULL UNIQUE,
+                    Password VARCHAR(255) NOT NULL,
+                    Email VARCHAR(255) NOT NULL,
+                    Role ENUM('so', 'user')
+                )
+            """)
+
+        # Check and create file table
+        mycursor.execute(f"SHOW TABLES LIKE 'file'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS file(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    User_ID INT,
+                    File_Meta_Data VARCHAR(255),
+                    File_Path VARCHAR(600),
+                    Co_Authors INT,
+                    File_Classification ENUM('non-sensitive', 'sensitive', 'confidential'),
+                    FOREIGN KEY (User_ID) REFERENCES user(ID) ON DELETE CASCADE
+                )
+            """)
+
+        # Check and create consent table
+        mycursor.execute(f"SHOW TABLES LIKE 'consent'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS consent(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    User_ID INT,
+                    Consent_Type VARCHAR(600),
+                    FOREIGN KEY (User_ID) REFERENCES user(ID) ON DELETE CASCADE
+                )
+            """)
+
+        # Check and create audit log table
+        mycursor.execute(f"SHOW TABLES LIKE 'audit_log'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS audit_log(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    User_ID INT,
+                    Action VARCHAR(255),
+                    Threat_Level ENUM('low', 'medium', 'high', 'critical') NOT NULL,
+                    Event_Time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (User_ID) REFERENCES user(ID) ON DELETE CASCADE
+                )
+            """)
+
+        # Check and create link table
+        mycursor.execute(f"SHOW TABLES LIKE 'link'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS link(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    User_ID INT,
+                    Expiration_Link VARCHAR(255) NOT NULL,
+                    Duration TIMESTAMP NOT NULL,
+                    Expired BOOLEAN DEFAULT FALSE,
+                    FOREIGN KEY (User_ID) REFERENCES user(ID) ON DELETE CASCADE
+                )
+            """)
+
+        # Check and create lock out table
+        mycursor.execute(f"SHOW TABLES LIKE 'lock_out'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS lock_out(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    User_ID INT,
+                    Locked BOOLEAN DEFAULT FALSE,
+                    Login_Attempts INT DEFAULT 0,
+                    Duration DATETIME,
+                    FOREIGN KEY (User_ID) REFERENCES user(ID) ON DELETE CASCADE
+                )
+            """)
+
+        # Check and create temp file sharing table
+        mycursor.execute(f"SHOW TABLES LIKE 'temp_file_sharing'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS temp_file_sharing(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    File_ID INT NOT NULL,
+                    Sharing_Link VARCHAR(255),
+                    Duration DATETIME,
+                    Privilege ENUM('read', 'edit'),
+                    Expired BOOLEAN DEFAULT FALSE,
+                    FOREIGN KEY (File_ID) REFERENCES file(ID) ON DELETE CASCADE
+                )
+            """)
+
+        # Check and create soft deletion table
+        mycursor.execute(f"SHOW TABLES LIKE 'soft_deletion'")
+        exist = mycursor.fetchone()
+        if not exist:
+            mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS soft_deletion(
+                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                    File_ID INT,
+                    File_Path VARCHAR(255),
+                    Duration TIMESTAMP,
+                    FOREIGN KEY (File_ID) REFERENCES file(ID) ON DELETE CASCADE
+                )
+            """)
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        mycursor.close()
+
+check_table()
+
